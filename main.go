5909de402f4d9ef309f6e38a2c708f6813fa3bf2
Ed <edward.dobson3@ntlworld.com>
2019-01-12 20:07:36 +0000

    Clean up files after running

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
 	"time"
 )
@@ -12,22 +13,29 @@ import (
 func main() {
 	http.HandleFunc("/ping", func(writer http.ResponseWriter, request *http.Request) {
 		currentTime := time.Now().UnixNano() / int64(time.Millisecond)
+		filePath := fmt.Sprintf("/tmp/%d.go", currentTime)
 		body, err := ioutil.ReadAll(request.Body)
 		if err != nil {
 			fmt.Fprintln(writer, "Error getting body: "+err.Error())
 			return
 		}
-		err = ioutil.WriteFile(fmt.Sprintf("/tmp/%d.go", currentTime), body, 0644)
+		err = ioutil.WriteFile(filePath, body, 0644)
 		if err != nil {
 			fmt.Fprintln(writer, "Error saving temp file: "+err.Error())
 			return
 		}
-		cmdOut, err := exec.Command("go", "run", fmt.Sprintf("/tmp/%d.go", currentTime)).CombinedOutput()
+		cmdOut, err := exec.Command("go", "run", filePath).CombinedOutput()
 		if err != nil {
 			fmt.Fprintln(writer, "Error running go file: "+err.Error())
 			return
 		}
 		fmt.Fprintln(writer, string(cmdOut))
+		go func() {
+			err := os.Remove(filePath)
+			if err != nil {
+				log.Printf("Error removing file %s: %s", filePath, err.Error())
+			}
+		}()
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
